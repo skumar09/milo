1326cd9dbd5f26f112f5df38f958b5e680b9a6b2
Santoshkumar Sharanappa Nateekar <nateekar@santoshumarsmbp.corp.adobe.com>
2024-10-04 16:51:10 -0700

    update the test1

--- nala/features/accessibility/accessibility.test.js
+++ nala/features/accessibility/accessibility.test.js
@@ -7,6 +7,9 @@ const testScope = process.env.TEST_SCOPE || 'body';
 const wcagTags = process.env.WCAG_TAGS ? process.env.WCAG_TAGS.split(',') : ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'];
 const maxViolations = process.env.MAX_VIOLATIONS ? parseInt(process.env.MAX_VIOLATIONS, 10) : 0;
 
+console.log(`Input URL: ${inputUrls}`);
+
+
 // Convert the input URLs into an array, trim and filter out empty URLs
 const urls = inputUrls.split(/[\s,]+/).map((url) => url.trim()).filter((url) => url);
 
